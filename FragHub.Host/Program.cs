e6f59b7e23f5c023427056bf9430f1ce280962f8
travis <travis@stanleytx.com>
2025-08-10 17:08:55 -0500

    rework

--- FragHub.Host/Program.cs
+++ FragHub.Host/Program.cs
@@ -3,9 +3,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using FragHub.Host.Extensions;
+using DotNetEnv;
 
 
 var builder = Host.CreateApplicationBuilder(args);
+
+Env.Load();
+Env.TraversePath().Load();
 
 builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Information));
 
